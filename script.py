b1f1c0760cb7c0ab892fcd6748c6998109fe136a
GunnarHe71 <gunnarhemming@hotmail.com>
2026-04-17 13:26:33 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,39 +1,25 @@
 import requests
 from datetime import datetime, timedelta
 
-URL = "https://data.brreg.no/enhetsregisteret/api/enheter"
+URL = "https://data.brreg.no/enhetsregisteret/api/oppdateringer/enheter"
 
-def hent_alle():
-    liste = []
-    url = URL + "?size=100"
-
-    while url:
-        r = requests.get(url)
-        data = r.json()
-
-        enheter = data.get("_embedded", {}).get("enheter", [])
-        if not enheter:
-            break
-
-        for e in enheter:
-            liste.append(e)
-
-        url = data.get("_links", {}).get("next", {}).get("href")
-
-    return liste
+def hent():
+    r = requests.get(URL + "?size=50")
+    data = r.json()
+    return data.get("_embedded", {}).get("oppdaterteEnheter", [])
 
 
 if __name__ == "__main__":
     now = datetime.now()
     grense = now - timedelta(days=30)
 
-    alle = hent_alle()
+    enheter = hent()
 
-    print("---- RESULTAT ----")
+    print("---- SISTE ENDRINGER ----")
 
     treff = 0
-    for e in alle:
-        dato_str = e.get("registreringsdato")
+    for e in enheter:
+        dato_str = e.get("oppdateringsdato")
         if not dato_str:
             continue
 
@@ -44,6 +30,6 @@ if __name__ == "__main__":
             treff += 1
 
     if treff == 0:
-        print("Ingen funnet (men scriptet fungerer)")
+        print("Ingen funnet (uvanlig)")
 
-    print("------------------")
+    print("-------------------------")
