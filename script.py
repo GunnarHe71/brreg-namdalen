e4e2bae73ec7075be4e60e6384c6411979216506
GunnarHe71 <gunnarhemming@hotmail.com>
2026-04-17 13:22:27 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,22 +1,49 @@
 import requests
+from datetime import datetime, timedelta
 
 URL = "https://data.brreg.no/enhetsregisteret/api/enheter"
 
-params = {
-    "size": 20,
-    "sort": "registreringsdato,desc"
-}
+def hent_alle():
+    liste = []
+    url = URL + "?size=100"
 
-r = requests.get(URL, params=params)
-data = r.json()
+    while url:
+        r = requests.get(url)
+        data = r.json()
 
-enheter = data.get("_embedded", {}).get("enheter", [])
+        enheter = data.get("_embedded", {}).get("enheter", [])
+        if not enheter:
+            break
 
-print("---- SISTE REGISTRERTE ----")
+        for e in enheter:
+            liste.append(e)
 
-for e in enheter:
-    navn = e.get("navn", "Ukjent")
-    dato = e.get("registreringsdato", "Ingen dato")
-    print(navn, "-", dato)
+        url = data.get("_links", {}).get("next", {}).get("href")
 
-print("---------------------------")
+    return liste
+
+
+if __name__ == "__main__":
+    now = datetime.now()
+    grense = now - timedelta(days=30)
+
+    alle = hent_alle()
+
+    print("---- RESULTAT ----")
+
+    treff = 0
+    for e in alle:
+        dato_str = e.get("registreringsdato")
+        if not dato_str:
+            continue
+
+        dato = datetime.fromisoformat(dato_str.replace("Z", "+00:00"))
+
+        if dato > grense:
+            print(e.get("navn"), "-", dato_str)
+            treff += 1
+
+    if treff == 0:
+        print("Ingen funnet (men scriptet fungerer)")
+
+    print("------------------")
